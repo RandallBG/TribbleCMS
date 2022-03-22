9eed46e61c3092ad999824f962f2a63fbb48af48
RandallBG <Randall.bg.gosnell@gmail.com>
2022-03-22 19:46:08 -0400

    added bootstrap styling for homepage

--- tritonfrontend/src/pages/Home.jsx
+++ tritonfrontend/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import './home.css'
+import Card from 'react-bootstrap/Card'
+
+import img1 from '../Images/img1.JPG'
+import img2 from '../Images/img2.JPG'
+import img3 from '../Images/img3.JPG'
 
 function Home() {
   return (
-    
+    <div>
         <div className="heroContainer">
           <div className="heroText">
             <h1>Triton</h1>
@@ -13,6 +18,32 @@ function Home() {
             </div>
 
         </div>
+        <div className="about-section">
+          <div className="imageFlex">
+            <Card>
+              <Card.Img variant="top" src={img1} />
+              <Card.Body>
+                <Card.Title className="text-center">Safety</Card.Title>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src={img2} />
+              <Card.Body>
+                <Card.Title className="text-center">Quality</Card.Title>
+              </Card.Body>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src={img3} />
+              <Card.Body>
+                <Card.Title className="text-center">Care</Card.Title>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="about-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente omnis ullam laborum ratione iusto quod aliquam culpa similique nihil reiciendis incidunt, provident dicta iste atque, rerum cumque. Quisquam, laudantium asperiores.
+          </div>
+        </div>
+    </div>
       
     
   )
